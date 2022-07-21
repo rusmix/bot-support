bd755e1cced5ab7fe4dc0c497efbf1ecc4268217
StasProfile <61944126+StasProfile@users.noreply.github.com>
2022-07-21 15:50:51 +0300

    added strategies

--- src/bot.js
+++ src/bot.js
@@ -1,24 +1,22 @@
-require('dotenv').config();
-const TelegramBot = require('node-telegram-bot-api');
-const userSide = require('../strategies/onMessage');
-const groupId = process.env.GROUP_ID;
-
+require("dotenv").config();
+const TelegramBot = require("node-telegram-bot-api");
+const BotStrategies = require("./botStrategies");
 
 class BotConfig {
-    constructor() {
-        this.telegramBot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
-        };
+  constructor() {
+    this.telegramBot = new TelegramBot(process.env.BOT_TOKEN, {
+      polling: true,
+    });
 
-    // initializeStrategies() {
-    //     this.telegramBot.onText(/\/start/, async (msg) => await onStart(msg, this.telegramBot));
+    this.strategies = new BotStrategies(this.telegramBot);
+  }
 
-    //     this.telegramBot.on("message", async (msg) => await onMessage(msg, this.telegramBot));
-    // }
-
-    initializeStrategies() {
-        this.telegramBot.on("message", async (msg) => await onMessage(msg, this.telegramBot));
-        console.log(123);
-    }
+  initializeStrategies() {
+    this.telegramBot.on(
+      "message",
+      async (msg) => await this.strategies.onMessage(msg)
+    );
+  }
 }
 
 module.exports = BotConfig;
--- src/botStrategies.js
+++ src/botStrategies.js
@@ -1,14 +1,59 @@
-const groupId = process.env.GROUP_ID;
+const groupId = +process.env.GROUP_ID;
 
 class BotStrategies {
-    constructor(telegramBot) {
-        this.telegramBot = telegramBot;
-    }
+  constructor(telegramBot) {
+    this.telegramBot = telegramBot;
+  }
 
-    onMessage = (msg) => {
-        userSide(msg, this.telegramBot);
-        groupSide(msg, this.telegramBot)
+  async onMessage(msg) {
+    console.log(msg.chat.id);
+    if (!msg.text) return;
+    if (msg.text.startsWith("/start")) return;
+
+    if (msg.chat.id !== groupId) return this.#handleMessageFromUser(msg);
+
+    return this.#handleMessageFromSupport(msg);
+  }
+
+  async #handleMessageFromUser(msg) {
+    const sentMessage = await this.telegramBot.sendMessage(
+      groupId,
+      `Message from: ${msg.chat.id}.\n\n${msg.text}`
+    );
+
+    if (!sentMessage)
+      return await this.telegramBot.sendMessage(
+        msg.chat.id,
+        "Возникла непредвиденная ошибка"
+      );
+    return await this.telegramBot.sendMessage(
+      msg.chat.id,
+      "Ваше сообщение успешно передано в поддержку"
+    );
+  }
+
+  async #handleMessageFromSupport(msg) {
+    if (!msg.text) return;
+    if (!msg.text.startsWith("/reply")) return;
+    try {
+      const [chatId, text] = [
+        +msg.text.split(" ")[1],
+        msg.text.split(" ").slice(2).join(" "),
+      ];
+      const sentReply = await this.telegramBot.sendMessage(chatId, text);
+      if (!sentReply)
+        return await this.telegramBot.sendMessage(
+          groupId,
+          "Ответ не отправлен, возникла ошибка на стороне telegram :("
+        );
+      return await this.telegramBot.sendMessage(groupId, "Ответ отправлен!");
+    } catch (e) {
+      return await this.telegramBot.sendMessage(
+        groupId,
+        `Не верно введена команда '/reply <chatId> <text>', текст ошибки: ${e.message}`
+      );
     }
+  }
 }
 
 module.exports = BotStrategies;
